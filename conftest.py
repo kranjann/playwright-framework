20c70d50790295654c362ab4a7a0ab458a69a3c8
Krishna Ranjan <krishna.ranjan.2021@gmail.com>
2026-06-30 19:18:00 +0530

    feat : Implement browser, context and page fixtures with proper lifecycle

--- conftest.py
+++ conftest.py
@@ -2,14 +2,29 @@ import pytest
 from playwright.sync_api import sync_playwright
 
 
-@pytest.fixture(scope="function")
-def page():
-
+@pytest.fixture(scope="session")
+def browser():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
 
-        page = browser.new_page()
-
-        yield page
+        yield browser
 
         browser.close()
+
+@pytest.fixture
+def context(browser):
+
+    context = browser.new_context()
+
+    yield context
+
+    context.close()
+
+@pytest.fixture
+def page(context):
+
+    page = context.new_page()
+
+    yield page
+
+    page.close()
